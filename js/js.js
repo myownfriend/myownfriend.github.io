65eab24a4d4a26eba863718a12e82d123b1681b2
myownfriend <obiwankenobi23@live.com>
2023-02-15 23:50:42 -0500

    Update to new dark mode behavior

--- js/js.js
+++ js/js.js
@@ -1,14 +1,47 @@
-import {Scene} from './scene.js';
+import {set_background, refresh} from './scene.js';
 import {Monitor} from './monitor.js';
 
 export const monitor = new Monitor();
-export const scene   = new Scene();
+const scene   = {
+	paintObjects : [
+				document.body,
+				document.getElementById('audiodropdown')
+				],
+	css : new CSSStyleSheet(),
+	wallpaper :  new Image(),
+	lights : [],
+	theme : (window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light',
+	stage : {
+		light : 1,
+		dark  : 0
+	}
+}
+
+function setMode() {
+	document.documentElement.id = scene.theme + "-mode";
+	document.getElementById('dark-mode-check').checked = scene.theme == 'dark' ? true : false;
+	refresh(scene);
+} setMode();
+
+//document.adoptedStyleSheets = [scene.css];
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => { 
+	scene.theme = (window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
+	setMode()
+});
+
+document.getElementById('dark-mode-check').addEventListener('change', () => {
+	scene.theme = (scene.theme == 'dark') ? 'light' : 'dark';
+	setMode()
+});
+
+scene.wallpaper.src = "img/wallpapers/RainDrops.jpg";
+
+set_background(scene);
 
 document.getElementById('activities').addEventListener('click', () => {
 	document.body.classList.toggle('overview');
+	//refresh(scene);
 });
-
-
 
 monitor.workspaces[0].addEventListener('dragover', (ev) => {
 	ev.preventDefault();
@@ -27,7 +60,8 @@ monitor.workspaces[0].addEventListener('drop', (ev) => {
 			if (allowedFiletypes.includes(ev.dataTransfer.items[0].type)) {
 				if (scene.wallpaper.src != null)
 					URL.revokeObjectURL(scene.wallpaper.src);
-				scene.set_background(URL.createObjectURL(ev.dataTransfer.items[0].getAsFile()));
+				scene.wallpaper.src = URL.createObjectURL(ev.dataTransfer.items[0].getAsFile())
+				set_background(scene);
 				ev.dataTransfer.items.clear();
 			}
 		}
