476c99c870fdc38426767b68fb6e1c9e7cef062e
myownfriend <obiwankenobi23@live.com>
2023-02-19 03:06:32 -0500

    Some changes don't seem to be sticking

--- js/monitor.js
+++ js/monitor.js
@@ -10,8 +10,8 @@ export class Monitor {
 		this.aspect = this.width / this.height;;
     }
 
-	update(scene) {
-		const
+    crop(scene) {
+        const
             scl = this.aspect > scene.aspect ? scene.wallpaper.width / this.width : scene.wallpaper.height / this.height,
             sx  = scene.wallpaper.width  - (this.width  * scl),
             sy  = scene.wallpaper.height - (this.height * scl),
@@ -19,10 +19,20 @@ export class Monitor {
             my  = sy / 2,
             mw  = scene.wallpaper.width  - sx,
             mh  = scene.wallpaper.height - sy;
+        return {
+            x : mx,
+            y : my,
+            w : mw,
+            h : mh
+        };
+    }
+
+	update(scene) {
+		const rect = this.crop(scene);
         for(const workspace of this.workspaces) {
             workspace.width  = this.width;
             workspace.height = this.height;
-			workspace.getContext('2d').drawImage(scene.wallpaper, mx, my, mw, mh,
+			workspace.getContext('2d').drawImage(scene.wallpaper, rect.x, rect.y, rect.w, rect.h,
                                                 0, 0, workspace.width, workspace.height);
 		}
 	}
