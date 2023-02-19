04e8319c4b579d54778859dbea91149695eaaaf4
myownfriend <obiwankenobi23@live.com>
2023-02-19 01:58:17 -0500

    Use pre-transformed vertex coordinates for fragment shader
    Instead of dividing fragment coordinates by the rect width and height to normalize coordinates:
    
    Send the vertices to the GPU in a 0-1.0 coordinate space,
    Make a copy of the coordinates before transform and transform the input vertices
    Send both to the fragment shader, the pretransformed coordinates are used for computing light distance. The other used for drawing the rect.
    
    This saves one divide per fragment.

--- scene.js
+++ scene.js
@@ -1,9 +1,131 @@
 import {monitor} from './js.js';
-import {createThumbnail} from './lerp.js';
+import {OKLAB_to_SRGB, SRGB_to_OKLAB} from './shaders.js';
+
+export class PaintObject {
+	constructor(surface) {
+		this.surface = surface;
+
+		this.canvas  = document.createElement('canvas');
+		this.canvas.classList.add('paint');
+		this.surface.prepend(this.canvas);
+
+		this.context = this.canvas.getContext('webgl2', {
+			depth     : false,
+			alpha     : false,
+			stencil   : false,
+			antialias : false,
+			preserveDrawingBuffer: true
+		});
+		this.program  = this.context.createProgram();
+		const
+			vShader  = this.context.createShader(this.context.VERTEX_SHADER),
+			fShader  = this.context.createShader(this.context.FRAGMENT_SHADER);
+
+		// wScale  = document.documentElement.clientWidth  / rect.width,
+		// hScale  = document.documentElement.clientHeight / rect.height,
+		// xOffset = rect.x / document.documentElement.clientWidth,
+		// yOffset = 1 - ((rect.y + rect.height) / document.documentElement.clientHeight),
+
+
+		this.context.shaderSource(vShader, `#version 300 es
+			precision mediump float;
+			in      vec2  aPosition;
+			uniform float surfaceColor;
+			// uniform vec4  rect;
+			uniform vec4  extra;
+			out     float brightness;
+			out     vec2  uv;
+			${SRGB_to_OKLAB}
+			void main() {
+				uv = aPosition * extra.xy;
+				brightness  = SRGB_to_OKLAB(vec3(surfaceColor / 255.0)).r;
+				gl_Position = mat4(
+					 2.0,  0.0,  0.0, 0.0,
+					 0.0,  2.0,  0.0, 0.0,
+					 0.0,  0.0,  1.0, 0.0,
+					-1.0, -1.0,  0.0, 1.0) * vec4(uv, 0.0, 1.0);
+			}`);
+		this.context.shaderSource(fShader, `#version 300 es
+			precision mediump float;
+			in float brightness;
+			in vec2 uv;
+			${OKLAB_to_SRGB}
+			struct light {
+				vec4  pos_color; // x, y, b, a
+				vec4 intensity;
+			};
+			layout(std140) uniform lighting {
+				light lights[6];
+			};
+			layout(location = 0) out vec4 Output;
+			void main() {
+				float i_acc   = 0.0;
+				vec2  ab_acc  = vec2(0.0, 0.0);
+				for (int i = 0; i < 6; i++) {
+					float intensity = lights[i].intensity.x / pow(distance(uv, lights[i].pos_color.xy), 2.0);
+					i_acc  += intensity;
+					ab_acc += intensity * lights[i].pos_color.ab;
+				}
+				vec3 OKLAB = vec3(brightness, ab_acc / i_acc);
+				Output = vec4(OKLAB_to_SRGB(OKLAB),1.0);
+			}`);
+		this.context.attachShader(this.program, vShader);
+		this.context.attachShader(this.program, fShader);
+		this.context.compileShader(vShader);
+		this.context.compileShader(fShader);
+		this.context.linkProgram(this.program);
+		this.context.useProgram(this.program);
+	}
+}
 
 export function refresh(scene) {
-	for(const object of scene.paintObjects)
+	for(const object of scene.paintObjects) {
+		const
+			surface    = object.canvas,
+			container  = object.surface.getBoundingClientRect();
+		surface.height = container.height;
+		surface.width  = container.width;
+		object.context.viewport(0,0,surface.width,surface.height);
 		drawLights(scene, object);
+	}
+}
+
+function drawLights(scene, obj) {
+	const
+		gl      = obj.context,
+		rect    = obj.surface.getBoundingClientRect(),
+		wScale  = document.documentElement.clientWidth  / rect.width,
+		hScale  = document.documentElement.clientHeight / rect.height,
+		xOffset = rect.x / document.documentElement.clientWidth,
+		yOffset = 1 - ((rect.y + rect.height) / document.documentElement.clientHeight),
+		ubObj   = [];
+
+	for (const light of scene.lights)
+		ubObj.push(
+			(light.x - xOffset) * wScale,
+			(light.y - yOffset) * hScale,
+			// light.x, light.y,
+			light.b, light.a,
+			light.i,     0.0,     0.0,    0.0);
+
+	gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, gl.createBuffer());
+	gl.bufferData(gl.UNIFORM_BUFFER, new Float32Array(ubObj), gl.STATIC_DRAW);
+	gl.uniformBlockBinding(obj.program, gl.getUniformBlockIndex(obj.program, 'lighting'), 0);
+
+	// If an alternative method of transforming the light position to the viewport is found,
+	// the any code after this may be the only data that needs be resent every frame
+	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0]), gl.STATIC_DRAW);
+
+	const aPosition = gl.getAttribLocation(obj.program, 'aPosition');
+	gl.enableVertexAttribArray(aPosition);
+	gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+	// If we don't need to send a new mesh every frame then THI
+	// is the only stuff that needs to be redone each frame
+	// gl.uniform4fv(gl.getUniformLocation(obj.program, "rect" ), new Float32Array([rect.x, rect.y, rect.width, rect.height]));
+	gl.uniform4fv(gl.getUniformLocation(obj.program, "extra"), new Float32Array([scene.aspect[0], scene.aspect[1],document.documentElement.clientWidth,document.documentElement.clientHeight]));
+	gl.uniform1f( gl.getUniformLocation(obj.program, "surfaceColor"), Number(window.getComputedStyle(obj.surface).getPropertyValue("background-color").split(', ')[1]));
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
 export function set_background(scene) {
@@ -14,7 +136,12 @@ export function set_background(scene) {
 
 	scene.wallpaper.addEventListener('load', () => {
 
-		scene.aspect = scene.wallpaper.width / scene.wallpaper.height;
+		const aspect = scene.wallpaper.width / scene.wallpaper.height;
+		scene.aspect = [1,1];
+		if(aspect > 1)
+			scene.aspect[0] *= aspect;
+		else
+			scene.aspect[1] /= aspect;
 
 		const
 			thumb = createThumbnail(scene.wallpaper),
@@ -32,126 +159,41 @@ export function set_background(scene) {
 
 	analyst.onmessage = (e) => {			
 		scene.lights = e.data.lights;
-		// As saturation goes down, move the top bar brightness towards white or black 
-		// I have no idea if this code is right. I'm tired.
+
 		scene.stage.dark  = Math.min(e.data.lum * e.data.sat, 0.25615);
 		scene.stage.light = Math.max(e.data.lum * (1 - e.data.sat), 1 - 0.25615);
-	
+
 		refresh(scene);
 	};
 }
 
-export function drawLights(scene, obj) {
-	const
-		cvs       = obj.firstChild,
-		ctx       = cvs.getContext('2d'),
-		L         = toOkLabBrightness(Number(window.getComputedStyle(obj).getPropertyValue("background-color").split(', ')[1]) / 255),
-		dispRect  = obj.getBoundingClientRect(),
-		scalew    = document.documentElement.clientWidth / dispRect.width,
-		scaleh    = document.documentElement.clientHeight / dispRect.height,
-		offsetx   = dispRect.x / document.documentElement.clientWidth,
-		offsety   = dispRect.y / document.documentElement.clientHeight,
-		lightSize = scene.lights.length,
-		lights    = structuredClone(scene.lights);
 
-	for (const light of lights) {
-		light.x = (light.x - offsetx) * scalew;
-		light.y = (light.y - offsety) * scaleh;
-	}
+function createThumbnail2(image, limit = 64) {
+	/*
+	 There might be a way to do the conversion from sRGB to OKLAB on the GPU
+	 then pass the render target to JS as an array. This should be quicker
+	 than converting pixels one at a time with JS.
+	 */
+    let scale = 1;
+    if (image.width * image.height > limit * limit)
+        scale = limit / (image.width / image.height > 1 ?  image.height : image.width);
 
-	for(let y = 0; y < cvs.height; y++) {
-		const fy = y / cvs.height;
-		for(let x = 0; x < cvs.width; x++) {
-			const fx = x / cvs.width;
-			let	i = 0, a = 0, b = 0;
-			for (let l = 0; l < lightSize; l++) {
-				const
-					distance  = Math.sqrt(((lights[l].x - fx)) ** 2 + ((lights[l].y - fy)) ** 2),
-					intensity = distance == 0 ? lights[l].i : lights[l].i / distance ** 2;
-				i += intensity;
-				a += intensity * lights[l].a;
-				b += intensity * lights[l].b;
-			}
-			const
-				OKLAB = [L / 1, a / i, b / i],
-				l = (1 * OKLAB[0] + 0.3963377774 * OKLAB[1] + 0.2158037573 * OKLAB[2]) ** 3,
-				m = (1 * OKLAB[0] - 0.1055613458 * OKLAB[1] - 0.0638541728 * OKLAB[2]) ** 3,
-				s = (1 * OKLAB[0] - 0.0894841775 * OKLAB[1] - 1.2914855480 * OKLAB[2]) ** 3,
-				rgb = [
-					+4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s,
-					-1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s,
-					-0.0041960863 * l - 0.7034186147 * m + 1.7076147010 * s
-				];
-			ctx.fillStyle = `rgb(${toSRGB(rgb[0]) * 255} ${toSRGB(rgb[1]) * 255} ${toSRGB(rgb[2]) * 255})`;
-			ctx.fillRect(x, y, 1, 1);
-		}
-	}
-
-	function toSRGB(i) {
-		if (i <= 0.0031308)
-			return 12.92 * i;
-		return 1.055 * Math.pow(i, 1/2.4) - 0.055;
-	}
+    const
+        // Dividing the dimensions by 2 and then multiplying the rounded result is intentional
+        // It makes sure that the result is rounded to the nearest even number.
+        // Someone smarter than me thought of this
+		// I'm not sure if this is strictly needed but made the media cut code fail less often
+        buf = new OffscreenCanvas(2 * Math.round(image.width * scale / 2), 2 * Math.round(image.height * scale / 2)),
+        ctx = buf.getContext('2d');
+        ctx.drawImage(image, 0, 0, buf.width, buf.height);
+    return buf;
 }
 
-export function drawLightsShader(scene, obj) {
-	const
-		gl        = obj.firstChild.getContext('webgl2'),
-		vBuffer   = gl.createBuffer(),
-		program   = gl.createProgram(),
-		aPosition = gl.getAttribLocation(program, 'aPosition'),
-		vShader   = gl.createShader(gl.VERTEX_SHADER),
-		fShader   = gl.createShader(gl.FRAGMENT_SHADER);
-
-	gl.uniform1f(brightness, toOkLabBrightness(Number(window.getComputedStyle(obj).getPropertyValue("background-color").split(', ')[1]) / 255));
-	gl.uniform1i(lightSize, scene.lights.length);
-	gl.uniform1f(aspect, scene.aspect);
-
-	gl.shaderSource(vShader, `
-		in vec3 aPosition;
-		void main() {
-			gl_Position = vec4(aPosition, 1.0);
-		}`);
-	gl.shaderSource(fShader, `
-		precision lowp float;
-
-		uniform float brightness;
-		uniform float aspect;
-		unfiorm int   lightSize;
-		void main() {
-			w   = vec2(0, 0);
-			ab  = vec2(0, 0);
-			for (int i; i < lightSize; i++) {
-				d = distance(gl_FragCoord.xy, lights[i].xy));
-				w  += d;
-				ab += w * lights[i].ab;
-			}
-			OKLAB = vec3(brightness, ab) / vec3(1, w);
-			vec3 XYZ = mat3(0.4887180, 0.3106803, 0.2006017, 0.1762044, 0.8129847, 0.0108109, 0.0000000, 0.0102048, 0.9897952) * OKLAB;
-			vec3 RGB = mat3( 3.2410450,-1.537138, -0.4986106, -0.9692440, 1.875968,  0.0415550, 0.0556352,-0.203977,  1.0569720) * XYZ;
-			vec3 sRGB = (RGB <= 0.0031308) ? RGB * 12.92 : pow(RGB, 1.0 / 2.4) * 1.055 - 0.055;
-		};`);
-
-	gl.compileShader(vShader);
-	gl.compileShader(fShader);
-	
-	gl.attachShader(program, vShader);
-	gl.attachShader(program, fShader);
-
-	gl.linkProgram(program);
-	gl.useProgram(program);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1.0,1.0,0.0, -1.0,-1.0,0.0, 1.0,1.0,0.0, 1.0,-1.0,0.0]), gl.STATIC_DRAW);
-
-	gl.enableVertexAttribArray(aPosition);
-	gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0, 0);
-
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
-
-function toOkLabBrightness(l) {
-	if (l <= 0.04045)
-		return Math.cbrt(l / 12.92);
-	return Math.cbrt(Math.pow((l + 0.055) / 1.055, 2.4));
+function createThumbnail(image, limit = 64) {
+    // THIS IS A FAKE FUNCTION FOR TESTING MEDIAN CUT
+    const
+        buf = new OffscreenCanvas(192 / 2, 128 / 2),
+        ctx = buf.getContext('2d');
+        ctx.drawImage(image, 0, 0, buf.width, buf.height);
+    return buf;
 }
