1b27372952ab2711b3390901e2ffab1a23b7de3a
myownfriend <obiwankenobi23@live.com>
2023-02-21 20:04:57 -0500

    Use aspect ratios for the  the background resize code
    This is in the preparation of moving this code to shaders. The hope is that this code can be re-used for the lights.
    
    This is my second time uploading this because I keep uploading to the wrong directory.

--- js/monitor.js
+++ js/monitor.js
@@ -9,29 +9,52 @@ export class Monitor {
         this.refresh();
 	}
 
+    constructor2() {
+        this.workspaces = []
+        for(let i = 0; i < 2; i++) {
+            const canvas = document.createElement('canvas');
+            document.getElementById('workspaces').appendChild(canvas);
+            this.workspaces.push(canvas.getContext('webgl2'));
+        }
+        this.refresh();
+	}
+
     refresh() {
         this.width  = document.documentElement.clientWidth;
         this.height = document.documentElement.clientHeight;
-		this.aspect = this.width / this.height;
+        this.aspect = [Math.max(1.0, this.width / this.height), Math.max(1.0, this.height / this.width)];
+
     }
 
 	update(scene) {
-        for(const obj of scene.paintObjects)
-            obj.context.uniform4fv(obj.context.getUniformLocation(obj.program, "area"), new Float32Array([this.aspect, 0.0, this.width, this.height]));
+        for(const obj of scene.paintObjects) {
+            const gl = obj.context;
+            gl.uniform4fv(obj.monitor, new Float32Array([this.aspect[0], this.aspect[1], this.width, this.height]));
+            // It's incorrect to compute the x and y offsets of the lights from screen's edge because the lights go off-screen
+	        // The wallpaper's aspect ratio needs to be taken into account.
+	        // Also since there's no resizable, movable surface's in the demom, position and rect never actually change in this unless the monitor is resized
+            // If we 
+	        gl.uniform4fv(obj.rect, new Float32Array([obj.surface.offsetLeft, this.height - (obj.surface.offsetTop + gl.canvas.height), gl.canvas.width, gl.canvas.height]));
+        }
+
+        // We only scale monitor coordinates to background texture coordinates. Both in floats
         const
-            w   = this.width  * window.devicePixelRatio,
-            h   = this.height * window.devicePixelRatio,
-            scl = this.aspect > scene.aspect ? scene.wallpaper.width / w : scene.wallpaper.height / h,
-            sx  = scene.wallpaper.width  - (w * scl),
-            sy  = scene.wallpaper.height - (h * scl),
-            mx  = sx / 2,
-            my  = sy / 2,
-            mw  = scene.wallpaper.width  - sx,
-            mh  = scene.wallpaper.height - sy;
+            scale = this.aspect[0] > scene.aspect[0] ? scene.aspect[0] / this.aspect[0] : scene.aspect[1] / this.aspect[1],
+            monitor_scaled  = [this.aspect[0] * scale, this.aspect[1] * scale],
+            float_w  = monitor_scaled[0] / scene.aspect[0],
+            float_h  = monitor_scaled[1] / scene.aspect[1],
+            float_x  = (1 - float_w) / 2,
+            float_y  = (1 - float_h) / 2;
+        const
+            pixel_x = float_x * scene.wallpaper.width ,
+            pixel_y = float_y * scene.wallpaper.height,
+            pixel_w = float_w * scene.wallpaper.width ,
+            pixel_h = float_h * scene.wallpaper.height;
+
         for(const workspace of this.workspaces) {
-            workspace.canvas.width  = w;
-            workspace.canvas.height = h;
-			workspace.drawImage(scene.wallpaper, mx, my, mw, mh,
+            workspace.canvas.width  = this.width  * window.devicePixelRatio;
+            workspace.canvas.height = this.height * window.devicePixelRatio;
+            workspace.drawImage(scene.wallpaper, pixel_x, pixel_y, pixel_w, pixel_h,
                                                 0, 0, workspace.canvas.width, workspace.canvas.height);
 		}
 	}
--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,4 @@
 import {monitor} from './js.js';
-import {OKLAB_to_SRGB, SRGB_to_OKLAB} from './shaders.js';
 
 export class PaintObject {
 	constructor(surface) {
@@ -18,51 +17,18 @@ export class PaintObject {
 		const
 			vShader   = this.context.createShader(this.context.VERTEX_SHADER),
 			fShader   = this.context.createShader(this.context.FRAGMENT_SHADER);
-		this.context.shaderSource(vShader, `#version 300 es
-			precision mediump float;
-			in      vec2  aPosition;
-			uniform float surfaceColor;
-			uniform vec4  rect;
-			uniform vec4  area;
-			out     float brightness;
-			out     vec2  uv;
-			${SRGB_to_OKLAB}
-			void main() {
-				vec2 s = area.zw / rect.zw;
-				vec2 t = rect.xy / area.zw;
-				uv = aPosition;
-				brightness  = SRGB_to_OKLAB(vec3(surfaceColor / 255.0)).r;
-				gl_Position = vec4((uv - t) * s * 2.0 - 1.0, 0.0, 1.0);
-			}`);
-		this.context.shaderSource(fShader, `#version 300 es
-			precision mediump float;
-			in float brightness;
-			in vec2 uv;
-			${OKLAB_to_SRGB}
-			struct light {
-				vec4 pos_color;
-				vec4 intensity;
-			};
-			layout(std140) uniform lighting {
-				light lights[6];
-			};
-			layout(location = 0) out vec4 Output;
-			void main() {
-				float i_acc  = 0.0;
-				vec2  ab_acc = vec2(0.0, 0.0);
-				for (int i = 0; i < 6; i++) {
-					float intensity = lights[i].intensity.x / pow(distance(uv, lights[i].pos_color.xy), 2.0);
-					i_acc  += intensity;
-					ab_acc += intensity * lights[i].pos_color.ab;
-				}
-				Output = vec4(OKLAB_to_SRGB(vec3(brightness, ab_acc / i_acc)),1.0);
-			}`);
+		this.context.shaderSource(vShader, vs_drawLights);
+		this.context.shaderSource(fShader, fs_drawLights);
 		this.context.attachShader(this.program, vShader);
 		this.context.attachShader(this.program, fShader);
 		this.context.compileShader(vShader);
 		this.context.compileShader(fShader);
 		this.context.linkProgram(this.program);
 		this.context.useProgram(this.program);
+
+		this.surfaceColor = this.context.getUniformLocation(this.program, "surfaceColor");
+		this.monitor = this.context.getUniformLocation(this.program, "monitor");
+		this.rect = this.context.getUniformLocation(this.program, "rect");
 
 		const aPosition = this.context.getAttribLocation(this.program, 'aPosition');
 		this.context.bindBuffer(this.context.ARRAY_BUFFER, this.context.createBuffer());
@@ -80,7 +46,9 @@ export function sendToAnalyze(scene) {
 		signal  = control.signal;
 
 	scene.wallpaper.addEventListener('load', () => {
-		scene.aspect = scene.wallpaper.width / scene.wallpaper.height;
+		//scene.aspect = scene.wallpaper.width / scene.wallpaper.height;
+
+		scene.aspect = [Math.max(1.0, scene.wallpaper.width / scene.wallpaper.height), Math.max(1.0, scene.wallpaper.height / scene.wallpaper.width)];
 		const
 			thumb = createThumbnail(scene.wallpaper),
 			ctx   = thumb.getContext('2d');
@@ -90,8 +58,6 @@ export function sendToAnalyze(scene) {
 			height: thumb.height,
 			image : ctx.getImageData(0, 0, thumb.width, thumb.height).data
 		});
-
-		monitor.update(scene);
 		control.abort();
 	}, {signal});
 
@@ -104,24 +70,47 @@ export function sendToAnalyze(scene) {
 			gl.bufferData(gl.UNIFORM_BUFFER, new Float32Array(e.data.lights), gl.STATIC_DRAW);
 			gl.uniformBlockBinding(object.program, gl.getUniformBlockIndex(object.program, 'lighting'), 0);
 		}
+		// This is only here because it contains the code that redraws the wallpaper
+		// This should moved into the refresh function, but only after we stop redrawing every frame
+		monitor.update(scene);
 	};
 }
 
 export function refresh(scene) {
-	for(const object of scene.paintObjects) {
-		const surface  = object.context.canvas;
-		surface.height = object.surface.clientHeight;
-		surface.width  = object.surface.clientWidth;
-		object.context.viewport(0,0,surface.width,surface.height);
-		drawLights(object);
+	for(const obj of scene.paintObjects) {
+		const
+			surface  = obj.context.canvas,
+			gl = obj.context;
+		surface.height = obj.surface.clientHeight;
+		surface.width  = obj.surface.clientWidth;
+		gl.viewport(0,0,surface.width,surface.height);
+		// draws lights
+		gl.uniform1f( obj.surfaceColor, Number(window.getComputedStyle(obj.surface).getPropertyValue("background-color").split(', ')[1]));
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 	}
 }
 
-function drawLights(obj) {
-	const gl = obj.context;
-	gl.uniform4fv(gl.getUniformLocation(obj.program, "rect"), new Float32Array([obj.surface.offsetLeft, monitor.height - (obj.surface.offsetTop + gl.canvas.height), gl.canvas.width, gl.canvas.height]));
-	gl.uniform1f( gl.getUniformLocation(obj.program, "surfaceColor"), Number(window.getComputedStyle(obj.surface).getPropertyValue("background-color").split(', ')[1]));
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+function full_refresh() {
+	for(const obj of scene.paintObjects) {
+		const gl = obj.context;
+		// It's incorrect to compute the x and y offsets of the lights from screen's edge because the lights go off-screen
+		// The wallpaper's aspect ratio needs to be taken into account.
+		// Also since there's no resizable, movable surface's in the demom, position and rect never actually change in this unless the monitor is resized
+		gl.uniform4fv(obj.rect, new Float32Array([obj.surface.offsetLeft, monitor.height - (obj.surface.offsetTop + gl.canvas.height), gl.canvas.width, gl.canvas.height]));
+	}
+	const 
+		buf = new OffscreenCanvas(scene.wallpaper.width, scene.wallpaper.height),
+		gl = buf.getContext('2d');
+		// wallpaper = gl.createTexture();
+
+		
+	for(const workspace of monitor.workspaces) {
+		workspace.canvas.width  = w;
+		workspace.canvas.height = h;
+		workspace.drawImage(scene.wallpaper, mx, my, mw, mh,
+											0, 0, workspace.canvas.width, workspace.canvas.height);
+	}
+	refresh();
 }
 
 function createThumbnail2(image, limit = 64) {
@@ -146,3 +135,96 @@ function createThumbnail(image, limit = 64) {
         ctx.drawImage(image, 0, 0, buf.width, buf.height);
     return buf;
 }
+
+// SHADER SECTION --------------------------------------------------------
+
+const OKLAB_to_SRGB = `
+    vec3 OKLAB_to_SRGB(vec3 OKLAB) {
+        vec3 LMS = mat3(
+            1.0000000000,  1.0000000000, 1.0000000000, 
+            0.3963377774, -0.1055613458,-0.0894841775, 
+            0.2158037573, -0.0638541728,-1.2914855480) * OKLAB;
+        vec3 RGB = mat3(
+             4.0767416621, -1.2684380046, -0.0041960863,
+            -3.3077115913,  2.6097574011, -0.7034186147,
+             0.2309699292, -0.3413193965,  1.7076147010) * pow(LMS, vec3(3.0));
+        return mix(1.055 * pow(RGB, vec3(1.0/2.4)) - 0.055, RGB * 12.92, lessThanEqual(RGB, vec3(0.0031308)));
+    }`;
+
+const SRGB_to_OKLAB = `
+    vec3 SRGB_to_OKLAB(vec3 sRGB) {
+        vec3 RGB = mix(pow((sRGB + 0.055) / 1.055, vec3(2.4)), sRGB / 12.92, lessThanEqual(sRGB,vec3(0.04045)));
+        vec3 LMS = mat3(
+             0.4122214708,  0.2119034982, 0.0883024619, 
+             0.5363325363,  0.6806995451, 0.2817188376, 
+             0.0514459929,  0.1073969566, 0.6299787005) * RGB;
+        return mat3(
+             0.2104542553,  1.9779984951, 0.0259040371,
+             0.7936177850, -2.4285922050, 0.7827717662,
+            -0.0040720468,  0.4505937099,-0.8086757660) * (sign(LMS) * pow(abs(LMS), vec3(0.3333333333333)));
+    }`;
+
+const fs_drawLights = `#version 300 es
+	precision mediump float;
+	in float brightness;
+	in vec2 uv;
+	${OKLAB_to_SRGB}
+	struct light {
+		vec4 pos_color;
+		vec4 intensity;
+	};
+	layout(std140) uniform lighting {
+		light lights[6];
+	};
+	layout(location = 0) out vec4 Output;
+	void main() {
+		float i_acc  = 0.0;
+		vec2  ab_acc = vec2(0.0, 0.0);
+		for (int i = 0; i < 6; i++) {
+			float intensity = lights[i].intensity.x / pow(distance(uv, lights[i].pos_color.xy), 2.0);
+			i_acc  += intensity;
+			ab_acc += intensity * lights[i].pos_color.ab;
+		}
+		Output = vec4(OKLAB_to_SRGB(vec3(brightness, ab_acc / i_acc)),1.0);
+	}`
+
+const vs_drawLights = `#version 300 es
+	precision mediump float;
+	in      vec2  aPosition;
+	uniform float surfaceColor;
+	uniform vec4  rect;
+	uniform vec4  monitor;
+	out     float brightness;
+	out     vec2  uv;
+	${SRGB_to_OKLAB}
+	void main() {
+		vec2 s = monitor.zw / rect.zw;
+		vec2 t = rect.xy / monitor.zw;
+		uv = aPosition;
+		brightness  = SRGB_to_OKLAB(vec3(surfaceColor / 255.0)).r;
+		gl_Position = vec4((uv - t) * s * 2.0 - 1.0, 0.0, 1.0);
+	}`;
+
+const vs_drawWallpaper = `#version 300 es
+	precision mediump float;
+	in      vec2  aPosition;
+	uniform vec4  wallpaperArea;
+	uniform vec4  monitor;
+	out     vec2 uv;
+	void main() {
+		vec2 s = monitor.zw / rect.zw;
+		vec2 t = rect.xy / monitor.zw;
+		gl_Position = vec4((uv - t) * s * 2.0 - 1.0, 0.0, 1.0);
+	}`;
+
+const fs_drawWallpaper = `#version 300 es
+	precision mediump float;
+	in      vec2  aPosition;
+	uniform float surfaceColor;
+	uniform vec4  rect;
+	uniform vec4  monitor;
+	void main() {
+		vec2 s = monitor.zw / rect.zw;
+		vec2 t = rect.xy / monitor.zw;
+		gl_Position = vec4((uv - t) * s * 2.0 - 1.0, 0.0, 1.0);
+	}`;
