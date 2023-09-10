3b83014222e37eb4fa1b320ff2c6108f4d8622da
myownfriend <obiwankenobi23@live.com>
2023-09-09 20:01:43 -0400

    Moved the sRGB to OkLab conversion to CPU
    Only Lightness needed to be computed so it didn't need a matrix transformation. Doing it on the CPU allows us to send the value as a uniform straight to the fragment shader instead of processing it in the vertex shader and sending it as a varying to the fragment shader. There's probably performance benefits to that.
    
    Ideally, we could just set the colors using OkLab in CSS and then animate and pull those values directly. Unfortunately, getComputedStyle only returns sRGB values.

--- js/scene.js
+++ js/scene.js
@@ -28,12 +28,12 @@ window.addEventListener('resize', () => {
 		objs[i].ctx.canvas.height = objs[i].ctx.canvas.parentElement.clientHeight;
 		objs[i].ctx.canvas.width  = objs[i].ctx.canvas.parentElement.clientWidth;
 		objs[i].ctx.uniform2fv(objs[i].scale, scale);
-		objs[i].ctx.uniform4fv(objs[i].rect, new Float32Array([
+		objs[i].ctx.uniform4fv(objs[i].rect, [
 			(objs[i].ctx.canvas.parentElement.offsetLeft + (objs[i].ctx.canvas.parentElement.offsetWidth  / 2.0) -  center_x) / width  * -2.0,
 			(objs[i].ctx.canvas.parentElement.offsetTop  + (objs[i].ctx.canvas.parentElement.offsetHeight / 2.0) -  center_y) / height *  2.0,
 			width  / objs[i].ctx.canvas.parentElement.offsetWidth,
 			height / objs[i].ctx.canvas.parentElement.offsetHeight
-		]));
+		]);
 		objs[i].ctx.viewport(0, 0, objs[i].ctx.canvas.width, objs[i].ctx.canvas.height);
 	}
 	update();
@@ -72,13 +72,19 @@ function update(length = 0) {
 
 	function refresh(timestamp) {
 		for (let i = 0; i < objs.length; i++) {
-			objs[i].ctx.uniform1f(objs[i].surfaceColor, window.getComputedStyle(objs[i].ctx.canvas).getPropertyValue("background-color").split(', ')[1] | 0);
+			objs[i].ctx.uniform1f(objs[i].brightness, getSurfaceBrightness(objs[i].ctx.canvas));
 			objs[i].ctx.drawArrays(objs[i].ctx.TRIANGLE_STRIP, 0, 4);
 		}
 		if (timestamp > animation_state.end)
 			return animation_state.active = false;
 		window.requestAnimationFrame(refresh);
 	}
+
+	function getSurfaceBrightness(surface) {
+		const val = (window.getComputedStyle(surface).getPropertyValue("background-color").split(', ')[1] | 0) / 255;
+		const abs = Math.abs(val);
+		return Math.cbrt((abs >= 0.04045) ? (val < 0 ? -1 : 1) * Math.pow((abs + 0.055) / 1.055, 2.2) : val / 12.92);
+   }
 }
 
 export function createLights(surface) {
@@ -90,7 +96,9 @@ export function createLights(surface) {
 			alpha     : false,
 			stencil   : false,
 			antialias : false,
+			desynchronized : true,
 			preserveDrawingBuffer: true,
+			powerPreference : "low-power",
 		}),
 	};
 	const program = data.ctx.createProgram();
@@ -98,37 +106,19 @@ export function createLights(surface) {
 	const fShader = data.ctx.createShader(data.ctx.FRAGMENT_SHADER);
 	data.ctx.shaderSource(vShader, `#version 300 es
 		precision mediump float;
-		in      vec2  vPosition;
-		in      vec2  tuv;
-		uniform vec4  rect;
-		uniform vec2  global_scale;
-		uniform vec2  background;
-		uniform float surfaceColor;
-		out     float brightness;
-		out     vec2  luv;
-		out     vec2  uv;
-		vec3 SRGB_to_OKLAB(vec3 sRGB) {
-			vec3 RGB = mix(pow((sRGB + 0.055) / 1.055, vec3(2.2)), sRGB / 12.92, lessThanEqual(sRGB,vec3(0.04045)));
-			vec3 LMS = mat3(
-					0.4122214708,  0.2119034982, 0.0883024619,
-					0.5363325363,  0.6806995451, 0.2817188376,
-					0.0514459929,  0.1073969566, 0.6299787005) * RGB;
-			return mat3(
-					0.2104542553,  1.9779984951, 0.0259040371,
-					0.7936177850, -2.4285922050, 0.7827717662,
-				-0.0040720468,  0.4505937099,-0.8086757660) * (sign(LMS) * pow(abs(LMS), vec3(0.3333333333333)));
-		}
+		in      mediump vec2  vPosition;
+		uniform mediump vec4  rect;
+		uniform mediump vec2  global_scale;
+		uniform mediump vec2  background;
+		out     mediump vec2  lxy;
 		void main() {
-			brightness  = SRGB_to_OKLAB(vec3(surfaceColor / 255.0)).r;
-			luv = vPosition * background;
-			uv  = tuv;
+			lxy = vPosition * background;
 			gl_Position = vec4((vPosition + rect.xy) * (global_scale * rect.zw), 1.0, 1.0);
 		}`);
 	data.ctx.shaderSource(fShader, `#version 300 es
 		precision mediump float;
-		in vec2 luv;
-		in float brightness;
-		uniform int length;
+		precision mediump int;
+		in vec2 lxy;
 		struct light {
 			vec4 pos_color;
 			vec4 intensity;
@@ -136,6 +126,8 @@ export function createLights(surface) {
 		layout(std140) uniform lighting {
 			light lights[6];
 		};
+		uniform float brightness;
+		uniform int length;
 		out vec4 color;
 		vec3 OKLAB_to_SRGB(vec3 OKLAB) {
 			vec3 LMS = mat3(
@@ -152,7 +144,7 @@ export function createLights(surface) {
 			float i_acc  = 0.0;
 			vec2  ab_acc = vec2(0.0, 0.0);
 			for (int i = 0; i < length; i++) {
-				float intensity = lights[i].intensity.x / pow(distance(luv, lights[i].pos_color.xy), 2.0);
+				float intensity = lights[i].intensity.x / pow(distance(lxy, lights[i].pos_color.xy), 2.0);
 				i_acc  += intensity;
 				ab_acc += intensity * lights[i].pos_color.ab;
 			}
@@ -165,17 +157,17 @@ export function createLights(surface) {
 	data.ctx.linkProgram(program);
 	data.ctx.useProgram(program);
 	data.ctx.enable(data.ctx.DITHER);
+	data.ctx.depthFunc(data.ctx.NEVER);
 	data.rect = data.ctx.getUniformLocation(program, "rect");
 	data.background = data.ctx.getUniformLocation(program, "background");
 	data.scale = data.ctx.getUniformLocation(program, "global_scale");
-	data.surfaceColor = data.ctx.getUniformLocation(program, "surfaceColor");
+	data.brightness = data.ctx.getUniformLocation(program, "brightness");
 	data.light_length = data.ctx.getUniformLocation(program, "length");
 	const vPosition = data.ctx.getAttribLocation(program, 'vPosition');
 	data.ctx.bindBuffer(data.ctx.ARRAY_BUFFER, data.ctx.createBuffer());
 	data.ctx.bufferData(data.ctx.ARRAY_BUFFER, new Float32Array([-1.0,1.0, -1.0,-1.0, 1.0,1.0, 1.0,-1.0]), data.ctx.STATIC_DRAW);
 	data.ctx.enableVertexAttribArray(vPosition);
 	data.ctx.vertexAttribPointer(vPosition, 2, data.ctx.FLOAT, false, 0, 0);
-	data.ctx.canvas.className = 'light';
 	data.ctx.bindBufferBase(data.ctx.UNIFORM_BUFFER, data.ctx.getUniformBlockIndex(program, "lighting"), data.ctx.createBuffer());
 	objs.push(data);
 }
